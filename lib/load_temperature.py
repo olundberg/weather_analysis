75c5b6657208d38a473306683df3d68447b76914
Oscar Lundberg <lundberg.oscar@gmail.com>
2019-03-18 20:05:58 +0100

    Added load_temp

---
+++ lib/load_temperature.py
@@ -0,0 +1,15 @@
+"""Load the temperature."""
+import pandas as pd
+import numpy as np
+
+def load_temperature():
+    """Import temperature data from SMHI."""
+
+    return df 
+
+
+
+
+if __name__ == '__main__':
+    """Test."""
+    df = load_temperature()
